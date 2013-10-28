86d733a34b7876b06674c42fcb2cd4f6de8acd0f
Paul Mathieu <paul.mathieu@pal-robotics.com>
2013-10-28 18:24:15 +0100

    play_motion: add service IsAlreadyThere

---
+++ scripts/is_already_there.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest("play_motion")
+import rospy
+import sensor_msgs.msg
+import play_motion.srv
+
+class IsAlreadyThere:
+    def __init__(self):
+        self.srv = rospy.Service("play_motion/is_already_there",
+                                 play_motion.srv.IsAlreadyThere,
+                                 self.is_already_there)
+        self.joint_sub = rospy.Subscriber("joint_states",
+                                          sensor_msgs.msg.JointState,
+                                          self.joint_state_cb)
+        self.joint_states = {}
+
+    def joint_state_cb(self, data):
+        for i in range(len(data.name)):
+            self.joint_states[data.name[i]] = data.position[i]
+
+    def is_already_there(self, data):
+        param = "play_motion/motions/{}".format(data.motion_name)
+        if not rospy.has_param(param):
+            rospy.logdebug("could not find motion {}".format(data.motion_name))
+            return False
+        motion = rospy.get_param(param)
+        for i in range(len(motion['joints'])):
+            joint_name = motion['joints'][i]
+            if joint_name not in self.joint_states:
+                rospy.logdebug("could not find state of joint {}".format(joint_name))
+                return False
+            if abs(motion['points'][0]['positions'][i]
+                   - self.joint_states[joint_name]) > data.tolerance:
+                rospy.logdebug("tolerance exceeded fot joint {}, {} vs {}"
+                              .format(joint_name,
+                                      motion['points'][0]['positions'][i],
+                                      self.joint_states[joint_name]))
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    rospy.init_node("is_already_there")
+    iat = IsAlreadyThere()
+    rospy.spin()
