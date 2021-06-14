411431483cba1b5548339544832c5d904b73e822
Jordan Palacios <jordan.palacios@pal-robotics.com>
2021-06-14 10:54:37 +0200

    rviz is now optionaly launched

--- play_motion/test/rrbot/rrbot.launch.py
+++ play_motion/test/rrbot/rrbot.launch.py
@@ -3,10 +3,22 @@ import xacro
 
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
 from launch_pal.include_utils import include_launch_py_description
 from launch_ros.actions import Node
+from launch.substitutions import LaunchConfiguration
 
 def generate_launch_description():
+
+    declared_arguments = []
+    declared_arguments.append(
+        DeclareLaunchArgument('rviz',
+                              default_value="False",
+                              description='Whether run rviz',
+                              choices=["True", "False"]))
+
+    rviz = LaunchConfiguration('rviz')
 
     robot_description_path = os.path.join(
         get_package_share_directory('play_motion'), 'test', 'rrbot.xacro')
@@ -40,13 +52,14 @@ def generate_launch_description():
         executable='rviz2',
         name='rviz2',
         output={'stdout': 'screen', 'stderr': 'log'},
-        arguments=['-d', rviz_config_file]
+        arguments=['-d', rviz_config_file],
+        condition=IfCondition(rviz)
     )
 
-    # @TODO rviz node should be optional
-    return LaunchDescription([
-        control_node,
-        controllers,
-        robot_state_publisher_node,
-        rviz_node
-    ])
+    launch_description = [control_node,
+                          controllers,
+                          robot_state_publisher_node,
+                          rviz_node]
+
+    return LaunchDescription(
+        declared_arguments + launch_description)
